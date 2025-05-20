1a86fcacc968b2e2ba09c4284a9a8019a19ad102
Pavithramutharasu <130883672+Pavithramutharasu@users.noreply.github.com>
2025-05-20 17:14:19 +0530

    Create JavaInttoString.java

---
+++ JavaInttoString.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaInttoString {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int n = input.nextInt();
+       String s = Integer.toString(n);
+       System.out.println("Good job");
+    }
+}
