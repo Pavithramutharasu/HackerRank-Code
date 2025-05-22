b30d9d2f48239972c37adf37a1bd859085889bd8
Pavithramutharasu <130883672+Pavithramutharasu@users.noreply.github.com>
2025-05-22 15:25:41 +0530

    Create Java1DArray.java

---
+++ Java1DArray.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Java1DArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        int [] arr = new int[n];
+        
+        for(int i = 0;i<n;i++){
+            arr[i] = Integer.parseInt(bufferedReader.readLine().trim());
+        }
+         
+        for(int i : arr){
+            System.out.println(i);
+        }
+            
+        bufferedReader.close();
+    }
+}
